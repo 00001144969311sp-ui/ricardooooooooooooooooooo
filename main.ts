dffab9aad7d5eb5789fb0a6ed3623e858cd7d5d2
00001144969311sp-ui <00001144969311sp@al.educacao.sp.gov.br>
2026-02-06 08:42:35 -0300

    0.0.1

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-	
+    music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 })
 basic.showIcon(IconNames.EighthNote)
